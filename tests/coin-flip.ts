52ef6722da6cd11ce0ff646f63c6f6c415ae996f
thrishank <thrishankkalluru@gmail.com>
2025-06-27 21:23:37 +0530

    update tests

--- tests/coin-flip.ts
+++ tests/coin-flip.ts
@@ -36,7 +36,17 @@ describe("coin-flip", () => {
     assert.ok(data.bump === bump);
   });
 
-  it("roll the dice", async () => {
+  let testContext: {
+    keypair: any;
+    connection: any;
+    program: any;
+    sbProgram: any;
+    queue: any;
+    randomness: any;
+    id: number;
+  };
+
+  it("create randomness account", async () => {
     const { keypair, connection, program } = await sb.AnchorUtils.loadEnv();
     const sbProgram = await loadSbProgram(program!.provider);
     let queue = await setupQueue(program);
@@ -64,6 +74,26 @@ describe("coin-flip", () => {
     console.log(" randomness account creation: ", sig1);
 
     const id = Math.floor(Math.random() * 1000000000);
+
+    testContext = {
+      keypair,
+      connection,
+      program,
+      sbProgram,
+      queue,
+      randomness,
+      id,
+    };
+  });
+
+  it("commit randomness and take funds from the user", async () => {
+    if (!testContext) {
+      throw new Error("Init test must run before reveal test");
+    }
+
+    const { randomness, id, keypair, connection, queue, sbProgram } =
+      testContext;
+
     const commitIx = await randomness.commitIx(queue);
 
     const coinFlipIx = await myProgram.methods
@@ -87,6 +117,14 @@ describe("coin-flip", () => {
     const sig4 = await connection.sendTransaction(commitTx);
     await connection.confirmTransaction(sig4);
     console.log("Transaction Signature commitTx", sig4);
+  });
+
+  it("reveal dice rool", async () => {
+    if (!testContext) {
+      throw new Error("Init test must run before reveal test");
+    }
+
+    const { randomness, id, keypair, connection, sbProgram } = testContext;
 
     const revealIx = await randomness.revealIx();
 
@@ -115,13 +153,7 @@ describe("coin-flip", () => {
     const answer = await connection.getParsedTransaction(sig5, {
       maxSupportedTransactionVersion: 0,
     });
-    let resultLog = answer?.meta?.logMessages?.filter((line) =>
-      line.includes("FLIP_RESULT")
-    )[0];
-    let result = resultLog?.split(": ")[2];
 
-    console.log("\nYour guess is ", true ? "Heads" : "Tails");
-
-    console.log(`\nAnd the random result is ... ${result}!`);
+    console.log("Answer: ", answer?.meta?.logMessages);
   });
 });
